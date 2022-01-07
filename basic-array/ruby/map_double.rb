38c62b5bacb72e17e16c05d5fd04a41cb6b15a45
Ismail Karabulut <i.karabulut93@gmail.com>
2022-01-07 15:32:35 -0600

    1/7/22 map:double ruby submission

---
+++ basic-array/ruby/map_double.rb
@@ -0,0 +1,28 @@
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+# Clarify
+# Visualize
+# Write
+# Debugger
+# Refactor
+
+# Input: [4, 2, 5, 99, -4]
+# Output: [8, 4, 10, 198, -8]
+
+# Since Each number of the input array needs to be doubled we would first need a new array variable to store the doubled number. 
+# Then we would need to create an index to go through each position in the array.
+# A while loop will be created to go through each index position in the first array. Within that loop the index value will be doubled and then that doubled number will get pushed into the new doubled_number array
+
+
+### 1/7/22 Submission using no built in methods ###
+def double(array)
+  doubled_number = []
+  i = 0
+  while i < array.length
+    doubled_number << array[i] * 2
+    i += 1
+  end
+  return doubled_number
+end
+
+p double([4, 2, 5, 99, -4])
