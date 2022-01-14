66f988ebe8074f20055ea82b6c272ee59e32a6f1
Ismail Karabulut <i.karabulut93@gmail.com>
2022-01-14 10:10:30 -0600

    1/14/22 show me the money algorithm ruby submission

---
+++ basic-string/ruby/show_me_the_money.rb
@@ -0,0 +1,28 @@
+# Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
+
+# 1 We will need to start by creating a method called money? which takes in a single string argument
+# 2 since we will have a string and want to see if a "$" is within the string we will need to iterate through the string so an index is needed and that index will have to start at position 0
+# 3 A loop will be needed to go through the entire input string.
+# 4 within that loop we will need an if statement to check if that position is a "$"
+# 5 if it is we can stop the loop right there and return true, if it is not it will go through the rest of the positions. 
+# 6 If nothing is a "$" then we will need a variable set to false at the beginning of the method.
+
+def money?(string)
+  i = 0
+  money = false
+  while i < string.length
+    if string[i] == "$"
+      money = true
+    end
+    i += 1
+  end
+  return money
+end
+
+p money?("i hate $ but i love money i know i know im crazy")
